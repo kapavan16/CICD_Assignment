cf71876b7392ac12cc55f1c908bdd262b470cb2b
kapavan16 <151057692+kapavan16@users.noreply.github.com>
2024-04-07 18:17:24 +0530

    reverting changes in order to comply with branch name

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
+import scikit
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
